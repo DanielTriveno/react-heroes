1bdef42c2ff3176423d2016586752b76135f739d
DanielTriveno <daniel.triveno@unmsm.edu.pe>
2024-04-15 23:37:39 -0500

    Tests on Navbar

---
+++ tests/ui/components/Navbar.test.jsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { AuthContext } from "../../../src/auth/context/AuthContext";
+import { MemoryRouter } from "react-router-dom";
+import { Navbar } from "../../../src/ui/components/Navbar";
+
+
+describe('Pruebas en <Navbar/>', () => { 
+
+    const contextValue = {
+        logged: true,
+        user: {
+            name: 'Daniel Huber'
+        },
+        logout: jest.fn()
+    }
+    
+    beforeEach( () => jest.clearAllMocks() );
+
+
+    test('Debe de mostrar el nombre del usuario', () => { 
+    
+        render(
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter>
+                    <Navbar/>
+                </MemoryRouter>
+            </AuthContext.Provider>
+
+        );
+        //screen.debug();
+        expect( screen.getByText('Daniel Huber') ).toBeTruthy();
+    });
+
+    test('Debe de llamar el logout y navigate cuando se hace click en el boton ', () => { 
+        
+    });
+
+});
